7c2e9ddf621b465b5b146d89cf7d8fc246439931
Lalomax <lalomax365@gmail.com>
2025-12-09 19:29:43 -0400

    fix passing props to Button component

--- 5 Component Props/js/components/AppButton.js
+++ 5 Component Props/js/components/AppButton.js
@@ -1,6 +1,6 @@
 export default {
     template: `
-                    <button class="bg-gray-400 hover:bg-gray-200 px-2 py-4 disabled:cursor-not-allowed" :disabled="processing">
+                    <button class="bg-gray-400 hover:bg-gray-200 px-2 py-4 disabled:cursor-not-allowed :disabled="processing">
                         <slot>
                     </button>
                     `,
