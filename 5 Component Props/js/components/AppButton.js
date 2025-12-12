5944bc01be1907a9a7571522eee3f1fd070937bc
Lalomax <lalomax365@gmail.com>
2025-12-12 09:53:41 -0400

    feat: changing props for Button component to change style button

--- 5 Component Props/js/components/AppButton.js
+++ 5 Component Props/js/components/AppButton.js
@@ -4,7 +4,10 @@ export default {
                         // //binding class you could use a string, an array, and an object
                         :class="{
                             // the applying key as css for the button element depends on the value to apply all css              
-                            'bg-gray-400 hover:bg-gray-200 px-2 py-4 disabled:cursor-not-allowed':true,
+                            ' border rounded px-5 py-2 disabled:cursor-not-allowed':true,
+                            'bg-blue-400 hover:bg-gray-200': type === 'primary',
+                            'bg-purple-400 hover:bg-gray-200': type === 'secondary',
+                            'bg-gray-400 hover:bg-gray-200': type === 'muted',
                         }" 
                         :disabled="processing">
                         <slot>
